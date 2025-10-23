842ff6cebe39816903c5a3bd654c13574a441403
Arthur Mariano <arthvm@proton.me>
2025-10-22 21:17:45 -0300

    feat(present): add initial presentation logic
    This commit introduces the core functionality for presenting slides from a buffer. It includes a parser to split content into slides based on markdown headers and uses floating windows to create the presentation UI.
    
    Key features include:
    - Parsing buffer content into a slide structure.
    - Displaying slides in a dedicated layout with header and body windows.
    - Keymaps for navigating between slides (`n`/`p`) and quitting (`q`).
    - Environment management to hide UI elements like `cmdheight` during the presentation and restore them on exit.

---
+++ lua/present.lua
@@ -0,0 +1,169 @@
+local M = {}
+
+local function create_floating_window(config)
+	-- Create a buffer
+	local buf = vim.api.nvim_create_buf(false, true) -- No file, scratch buffer
+
+	-- Create the floating window
+	local win = vim.api.nvim_open_win(buf, true, config)
+
+	return { buf = buf, win = win }
+end
+
+M.setup = function()
+	-- nothing
+end
+
+---@class present.Slides
+---@fields slides present.Slide[]: The slides of the file
+
+---@class present.Slide
+---@fields title string: The title of the slide
+---@fields body string[]: The body of the slide
+
+--- Takes some lines and parses them
+---@param lines string[]: The lines in the buffer
+---@return present.Slides
+local parse_slides = function(lines)
+	local slides = { slides = {} }
+	local current_slide = {
+		title = "",
+		body = {},
+	}
+
+	local separator = "^#"
+
+	for _, line in ipairs(lines) do
+		if line:find(separator) then
+			if #current_slide.title > 0 then
+				table.insert(slides.slides, current_slide)
+			end
+
+			current_slide = {
+				title = line,
+				body = {},
+			}
+		else
+			table.insert(current_slide.body, line)
+		end
+
+		table.insert(current_slide, line)
+	end
+
+	table.insert(slides.slides, current_slide)
+	return slides
+end
+
+M.start_presentation = function(opts)
+	opts = opts or {}
+	opts.bufnr = opts.bufnr or 0
+
+	local lines = vim.api.nvim_buf_get_lines(opts.bufnr, 0, -1, false)
+	local parsed = parse_slides(lines)
+
+	---@type vim.api.keyset.win_config
+	local width = vim.o.columns
+	local height = vim.o.lines
+	local windows = {
+		background = {
+			relative = "editor",
+			width = width,
+			height = height,
+			style = "minimal",
+			col = 0,
+			row = 0,
+			zindex = 1,
+		},
+		header = {
+			relative = "editor",
+			width = width,
+			height = 1,
+			style = "minimal",
+			col = 0,
+			row = 0,
+			border = "rounded",
+			zindex = 2,
+		},
+		body = {
+			relative = "editor",
+			width = width - 8,
+			height = height - 5,
+			style = "minimal",
+			col = 8,
+			row = 4,
+			border = { " ", " ", " ", " ", " ", " ", " ", " " },
+			zindex = 2,
+		},
+		-- footer = {}
+	}
+
+	local background_float = create_floating_window(windows.background)
+	local header_float = create_floating_window(windows.header)
+	local body_float = create_floating_window(windows.body)
+
+	vim.bo[header_float.buf].filetype = "markdown"
+	vim.bo[body_float.buf].filetype = "markdown"
+
+	local set_slide_content = function(idx)
+		local slide = parsed.slides[idx]
+
+		local padding = string.rep(" ", (width - #slide.title) / 2)
+		local title = padding .. slide.title
+
+		vim.api.nvim_buf_set_lines(header_float.buf, 0, -1, false, { title })
+		vim.api.nvim_buf_set_lines(body_float.buf, 0, -1, false, slide.body)
+	end
+
+	local current_slide = 1
+	vim.keymap.set("n", "n", function()
+		current_slide = math.min(current_slide + 1, #parsed.slides)
+		set_slide_content(current_slide)
+	end, {
+		buffer = body_float.buf,
+	})
+
+	vim.keymap.set("n", "p", function()
+		current_slide = math.max(current_slide - 1, 1)
+		set_slide_content(current_slide)
+	end, {
+		buffer = body_float.buf,
+	})
+
+	vim.keymap.set("n", "q", function()
+		vim.api.nvim_win_close(body_float.win, true)
+	end, {
+		buffer = body_float.buf,
+	})
+
+	local restore = {
+		cmdheight = {
+			original = vim.o.cmdheight,
+			present = 0,
+		},
+	}
+
+	-- This is kinda cool
+	-- Basically setting the options of the user and the options we want for presentation
+	for option, config in pairs(restore) do
+		vim.opt[option] = config.present
+	end
+
+	vim.api.nvim_create_autocmd("BufLeave", {
+		buffer = body_float.buf,
+		callback = function()
+			-- Here we reset the options back to the original
+			for option, config in pairs(restore) do
+				vim.opt[option] = config.original
+			end
+
+			pcall(vim.api.nvim_win_close, background_float.win, true)
+			pcall(vim.api.nvim_win_close, header_float.win, true)
+		end,
+	})
+
+	set_slide_content(current_slide)
+end
+
+M.start_presentation({ bufnr = 15 })
+
+return M
